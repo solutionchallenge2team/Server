4369483993d6ac8dbd647da0a47537670aef00cb
bomink <bomink2004@gmail.com>
2024-02-20 03:49:07 +0900

    photos

--- src/auth/dto/create-board-dto.ts
+++ src/auth/dto/create-board-dto.ts
@@ -2,13 +2,14 @@ import { IsNotEmpty } from "class-validator";
 import { Reply } from "src/reply/reply.entity";
 
 export class CreateBoardDto {
-    //@IsNotEmpty()
+    @IsNotEmpty()
     title: string;      //제목
 
-    //@IsNotEmpty()
+    @IsNotEmpty()
     content: string;    //설명
 
-    //@IsNotEmpty()        
+    @IsNotEmpty()        
     location: string;   //위치
 
+    photos: string[];
 }
