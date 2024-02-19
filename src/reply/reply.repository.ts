114485af93c1a064514b20f5edf4ec6f03617d8b
bomink <bomink2004@gmail.com>
2024-02-19 17:29:59 +0900

    reply 추가

--- src/reply/reply.repository.ts
+++ src/reply/reply.repository.ts
@@ -2,15 +2,17 @@ import { Repository } from 'typeorm';
 import { Reply } from './reply.entity';
 import { CustomRepository } from 'src/db/typeorm-ex.decorator';
 import { CreateReplyDto } from 'src/auth/dto/create-reply-dto';
+import { Board } from 'src/boards/board.entity';
 
 @CustomRepository(Reply)
 export class ReplyRepository extends Repository<Reply> {
     
-    async createReply(createReplyDto: CreateReplyDto): Promise<Reply> {
+    async createReply(createReplyDto: CreateReplyDto, board: Board): Promise<Reply> {
         const { replyContent } = createReplyDto;
         
         const Reply = this.create({
-            replyContent
+            replyContent,
+            board,
         })
         
         await this.save(Reply);
